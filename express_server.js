027ed8e830592d078f0bb2122e469c5ae31f10c3
Hardeep Kaur Aulakh <aulakh.hardeep91@gmail.com>
2023-05-12 17:44:52 -0600

    Add express_server.js

--- express_server.js
+++ express_server.js
@@ -15,6 +15,10 @@ app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);//here getting the urlDatabase object in the form of json string
 });
 
+app.get("/hello", (req, res) => {
+  res.send("<html><body>Hello <b>World</b></body></html>\n");
+});
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
