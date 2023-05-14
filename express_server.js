c8ccccf9f11595243381469f37cd8897eb81269b
Hardeep Kaur Aulakh <aulakh.hardeep91@gmail.com>
2023-05-13 18:57:32 -0600

    Modify post request

--- express_server.js
+++ express_server.js
@@ -39,7 +39,8 @@ app.get("/urls/:id", (req, res) => {
 
 app.post("/urls", (req, res) => {
   console.log(req.body); // Log the POST request body to the console
-  res.send(generateRandomString()); // Respond with 'Ok' (we will replace this)
+  generateRandomString();
+  res.send('OK'); // Respond with 'Ok' (we will replace this)
 });
 
 
